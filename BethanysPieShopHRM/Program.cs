87fc1ef8f17683964db6cad0cbb9ca158a1bb014
Lenovo <Lenovo@LAPTOP-OP3N12H0>
2023-09-12 14:26:17 +0200

    Add project files.

---
+++ BethanysPieShopHRM/Program.cs
@@ -0,0 +1,11 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+Console.WriteLine("Hello, World!");
+
+Console.WriteLine("Please enter your First Name");
+string firstName = Console.ReadLine();
+
+Console.WriteLine("Please enter your Last Name");
+string lastName = Console.ReadLine();
+
+Console.WriteLine("Hello " + firstName + " " + lastName);
